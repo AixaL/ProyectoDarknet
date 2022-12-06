d96550bdc64df080784029b3bc6a636e5b06efe4
Aixa <aixa.lievana@gmail.com>
2022-12-06 13:45:53 -0600

    formulario

--- src/proyectodarknet/Formulario.java
+++ src/proyectodarknet/Formulario.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.io.File;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -45,27 +46,26 @@ public class Formulario extends JFrame implements ActionListener {
     private JButton addC;
     
     private String operadores[] = { "=", ">", "<"};
+    private Set<Columna> columnasRequeridas1 =  new HashSet<Columna>();
+    private int posicion1 = 0;
+    private Tipo tipo1 = Tipo.DOUBLE;
+    private Operador operador1 = Operador.EQUALS;
     
-//    private String numeradores[] = {'a','b','c','d','e'};
- 
- 
     ArrayList<String> columnasName = new ArrayList<>();
     ArrayList<Columna> columnas = new ArrayList<>();
     ArrayList<String> columnasId = new ArrayList<>();
     ArrayList<String> columnasSel = new ArrayList<>();
-    // constructor, to initialize the components
-    // with default values.
-    public Formulario()
-    {
+   
+
+
+    public Formulario(){
         for (Columna columna: Columna.values()) {
                 System.out.println(columna);
                 columnasName.add(columna.getNombre());
                 columnas.add(columna);
-//                columnasId.add(columna.getPosicion());
         }
-        System.out.println(columnas);
         
-        setTitle("Registration Form");
+        setTitle("Proyecto Final");
         setBounds(300, 90, 900, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
@@ -73,7 +73,7 @@ public class Formulario extends JFrame implements ActionListener {
         c = getContentPane();
         c.setLayout(null);
  
-        titulo = new JLabel("Registration Form");
+        titulo = new JLabel("Proyecto Final");
         titulo.setFont(new Font("Arial", Font.PLAIN, 30));
         titulo.setSize(300, 30);
         titulo.setLocation(300, 30);
@@ -84,8 +84,15 @@ public class Formulario extends JFrame implements ActionListener {
         rutaText.setSize(350, 20);
         rutaText.setLocation(100, 100);
         c.add(rutaText);
- 
-        ruta = new JTextField();
+
+        File archivoE = new File(getRutaArchivoEntrada());
+
+        if(archivoE.exists()){
+            System.out.println(getRutaArchivoEntrada());
+            ruta = new JTextField(getRutaArchivoEntrada());
+        }else{
+            ruta = new JTextField();
+        }
         ruta.setFont(new Font("Arial", Font.PLAIN, 15));
         ruta.setSize(350, 40);
         ruta.setLocation(100, 130);
@@ -120,7 +127,7 @@ public class Formulario extends JFrame implements ActionListener {
        
  
         criT = new JLabel("Criterio de busqueda");
-        criT.setFont(new Font("Arial", Font.PLAIN, 16));
+        criT.setFont(new Font("Arial", Font.PLAIN, 20));
         criT.setSize(400, 20);
         criT.setLocation(100, 330);
         c.add(criT);
@@ -129,12 +136,30 @@ public class Formulario extends JFrame implements ActionListener {
         cri.setFont(new Font("Arial", Font.PLAIN, 15));
         cri.setSize(180, 40);
         cri.setLocation(100, 370);
+        cri.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent arg0) {
+                //Do Something
+                System.out.println(cri.getSelectedItem());
+                if(cri.getSelectedItem()=="flow_id" || cri.getSelectedItem()=="src_ip" || cri.getSelectedItem()=="dst_ip" || cri.getSelectedItem() == "timestamp" || cri.getSelectedItem() == "label1" || cri.getSelectedItem()== "label2"){
+                    op.setSelectedIndex(0);
+                }
+            }
+        });
         c.add(cri);
         
         op = new JComboBox(operadores);
         op.setFont(new Font("Arial", Font.PLAIN, 15));
         op.setSize(50, 40);
         op.setLocation(280, 370);
+        op.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent arg0) {
+                //Do Something
+                System.out.println(cri.getSelectedItem());
+                if(cri.getSelectedItem()=="flow_id" || cri.getSelectedItem()=="src_ip" || cri.getSelectedItem()=="dst_ip" || cri.getSelectedItem() == "timestamp" || cri.getSelectedItem() == "label1" || cri.getSelectedItem()== "label2"){
+                    op.setSelectedIndex(0);
+                }
+            }
+        });
         c.add(op);
  
         criText = new JTextField();
@@ -147,9 +172,9 @@ public class Formulario extends JFrame implements ActionListener {
         term.setFont(new Font("Arial", Font.PLAIN, 15));
         term.setSize(250, 20);
         term.setLocation(150, 420);
-        c.add(term);
+        // c.add(term);
  
-        sub = new JButton("Submit");
+        sub = new JButton("Filtrar");
         sub.setFont(new Font("Arial", Font.PLAIN, 15));
         sub.setSize(100, 20);
         sub.setLocation(150, 470);
@@ -168,7 +193,6 @@ public class Formulario extends JFrame implements ActionListener {
         tout.setFont(new Font("Arial", Font.PLAIN, 15));
         tout.setSize(300, 400);
         tout.setLocation(500, 100);
-//        tout.setLineWrap(true);
         tout.setEditable(false);
         c.add(tout);
  
@@ -187,143 +211,126 @@ public class Formulario extends JFrame implements ActionListener {
  
         setVisible(true);
     }
- 
-    // method actionPerformed()
-    // to get the action performed
-    // by the user and act accordingly
+
     public void actionPerformed(ActionEvent e)
     {
         if (e.getSource() == addC) {
-                String data2 =  (String)colInteres.getSelectedItem();
-
-                
-                System.out.println(label.getText());
-                
+                String data2 =  (String)colInteres.getSelectedItem();     
                 String texto = label.getText().replaceAll("</html>", "");
                 texto = texto.replaceAll("<html>", "");
-
-                
                 texto = texto + " / " + data2;
-                
                 columnasSel.add(data2);
-                
-//                String data3 = "Address : " + tadd.getText();
-                
                 label.setText(label.getText() +" "+ data2);
                 final String html = "<html>"+ texto +"</html>";
                 label.setText(html);
-//                tout.setText(data + data1 + data2 + data3);
-//                tout.setEditable(false);
-//                res.setText("Registration Successfully..");
-//            }
-//            else {
-//                tout.setText("");
-//                resadd.setText("");
-//                res.setText("Please accept the"
-//                            + " terms & conditions..");
-//            }
         }
         
         else if(e.getSource() == sub){
 
-            Set<Columna> columnasRequeridas1 =  new HashSet<Columna>();
-            int posicion1 = 0;
-            Tipo tipo1 = Tipo.DOUBLE;
-//            tout.append( "Columnas de interes \n");
             String list = "";
              for (int i=0;i<columnasSel.size();i++) {
-                    System.out.println(columnasSel.get(i));
                      for (int j=0;j<columnas.size();j++) {
                         if(columnas.get(j).getNombre() == columnasSel.get(i) ){
-                            System.out.println(columnas.get(j).getTipo());
                             columnasRequeridas1.add(columnas.get(j));
                         }
                         
                     }
                     list = list + "<li>" + columnasSel.get(i) + "</li>";
                   }
-//            tout.append( "Ruta del archivo \n");
             
-            System.out.print(columnasRequeridas1);
-//          tout.append( "Criterio de filtrado \n");
             String criterioB = (String)cri.getSelectedItem()+ " " +(String) op.getSelectedItem()+ " " + criText.getText();
-            
-            tout.setText("<h2>Datos finales </h2>"
+
+            System.out.println(ruta.getText());
+
+            String ruuta = (ruta.getText().length()<=1)?"Ingresa una ruta del archivo":ruta.getText();
+
+
+            File archivoF = new File(ruuta);
+
+            if(list.length() > 1 && criterioB.length()>1 && ruta.getText().length()>2 && archivoF.exists()){
+                tout.setText("<h2>Datos finales </h2>"
                     + "<h4>Columnas:</h4>"
                     + "<ul>"+list+ "</ul>"
                     + "<h4>Criterio de busqueda</h4>"
                     + "<p>"+criterioB+"</p>"
                     + "<h4>Ruta del archivo: </h4>"
-                    + "<p>"+ruta.getText()+"</p>");
-
-            // for (int i=0;i<columnasSel.size();i++) {
-            //     System.out.println(columnasSel.get(i));
-            //     list = list + "<li>" + columnasSel.get(i) + "</li>";
-            // }
-
-
+                    + "<span>"+ruuta+"</span>");
            
-            for (int j=0;j<columnas.size();j++) {
-                if(columnas.get(j).getNombre() == cri.getSelectedItem() ){
-                    posicion1 = columnas.get(j).getPosicion();
-                    tipo1 = columnas.get(j).getTipo();
+                for (int j=0;j<columnas.size();j++) {
+                    if(columnas.get(j).getNombre() == cri.getSelectedItem() ){
+                        posicion1 = columnas.get(j).getPosicion();
+                        tipo1 = columnas.get(j).getTipo();
+                    }
+                    
                 }
+
+                if(op.getSelectedItem()=="<"){
+                    operador1   = Operador.LESSTHAN;
+                }
+                if(op.getSelectedItem()==">"){
+                    operador1   = Operador.MORETHAN;
+                }
+                if(op.getSelectedItem()=="="){
+                    operador1   = Operador.EQUALS;
+                }
+
+                String      valor1      = criText.getText(); // Valor aceptado
                 
-            }
+                Filtro[] filtros1 = {
+                    new Filtro(
+                        posicion1,
+                        operador1,
+                        valor1,
+                        tipo1
+                    )
+                };
 
+                if (!ruta.getText().isBlank()) {
+                    setRutaArchivoEntrada(ruta.getText());
+                    System.out.println("Nueva ruta: " + getRutaArchivoEntrada());
+                }
 
-            // int         posicion1   = posicion1.getPosicion();
-            Operador    operador1   = Operador.MORETHAN;
-            String      valor1      = "40000"; // Valor aceptado
+                try {
+                    Archivo.procesarArchivo(getRutaArchivoEntrada());
+                } catch (ErrorArchivo ex) {
+                    Logger.getLogger(Formulario.class.getName()).log(Level.SEVERE, null, ex);
+                }
             
-            Filtro[] filtros1 = {
-                new Filtro(
-                    posicion1,
-                    operador1,
-                    valor1,
-                    tipo1
-                )
-            };
-
-
-
-            if (!ruta.getText().isBlank()) {
-                setRutaArchivoEntrada(ruta.getText());
-                System.out.println("Nueva ruta: " + getRutaArchivoEntrada());
+                try {
+                    new Manager( filtros1 , columnasRequeridas1 );
+                } catch (ErrorEscritura ex) {
+                System.out.println(ex);
+                } catch (InterruptedException ex) {
+                    System.out.println(ex);
+                    // Logger.getLogger(Formulario.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ErrorArchivo ex) {
+                    System.out.println(ex);
+                    // Logger.getLogger(Formulario.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }else{
+                tout.setText("<h2>Datos finales </h2>"
+                    + "<h4>Columnas:</h4>"
+                    + "<ul>"+list+ "</ul>"
+                    + "<h4>Criterio de busqueda</h4>"
+                    + "<p>"+criterioB+"</p>"
+                    + "<h4>Ruta del archivo: </h4>"
+                    + "<p>"+ruuta+"</p>"
+                    + "<h2>Hubo un error. Ingresa bien los datos</h2>");
             }
-
-            try {
-                Archivo.procesarArchivo(getRutaArchivoEntrada());
-            } catch (ErrorArchivo ex) {
-                Logger.getLogger(Formulario.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-
+            
             
            
-            try {
-                new Manager( filtros1 , columnasRequeridas1 );
-            } catch (ErrorEscritura ex) {
-               System.out.println(ex);
-            } catch (InterruptedException ex) {
-                System.out.println(ex);
-                // Logger.getLogger(Formulario.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ErrorArchivo ex) {
-                System.out.println(ex);
-                // Logger.getLogger(Formulario.class.getName()).log(Level.SEVERE, null, ex);
-            }
-           
-            
-            String rutan =  ruta.getText();
-            
-            Archivo read = new Archivo();
         }
  
         else if (e.getSource() == reset) {
             String def = "";
-            ruta.setText(def);
+            File archivoE = new File(getRutaArchivoEntrada());
+            if(archivoE.exists()){
+                ruta.setText(getRutaArchivoEntrada());
+            }else{
+                ruta.setText(def);
+            }
             criText.setText(def);
-//            cri.setText(def);
             label.setText("Columnas: ");
             tout.setText(def);
             term.setSelected(false);
@@ -331,6 +338,8 @@ public class Formulario extends JFrame implements ActionListener {
             colInteres.setSelectedIndex(0);
             op.setSelectedIndex(0);
             resadd.setText(def);
+            columnasSel= new ArrayList<>();
+            columnasRequeridas1 =  new HashSet<Columna>();
         }
     }
 }
