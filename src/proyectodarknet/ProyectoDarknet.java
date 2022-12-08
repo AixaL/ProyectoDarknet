ed341c64fb85196636278267ed1441f9ffd61e9c
Coca Cola de Piña <kyle51297@gmail.com>
2022-12-07 19:21:10 -0600

    Limpieza

--- src/proyectodarknet/ProyectoDarknet.java
+++ src/proyectodarknet/ProyectoDarknet.java
@@ -4,21 +4,27 @@ import constantes.Constantes;
 
 import java.io.File;
 
-import static constantes.ConstantesDeEjecucion.getRutaDirectorioEntrada;
+import static archivos.Herramientas.Herramientas.vaciarDirectorio;
+import static constantes.ConstantesDeEjecucion.*;
 
 public class ProyectoDarknet implements Constantes {
 
     public static void main(String[] args) throws Exception {
-
-        new Formulario();
-
         System.out.println("CPUs disponibles: " + Hilos);
 
         File directorioEntrada = new File(getRutaDirectorioEntrada());
         if (directorioEntrada.mkdirs()){
             System.out.println("Directorio creado: " + directorioEntrada.getAbsolutePath());
         }
-        
+
+        File directorioSalida = new File(getRutaDirectorioSalida());
+        if (directorioSalida.mkdirs()){
+            System.out.println("Directorio creado: " + directorioSalida.getAbsolutePath());
+        }
+
+        vaciarDirectorio(getRutaDirectorioProceso());
+
+        new Formulario();
     }
     
 }
